f876d760bd3d38eb223d36b2bc7ca913fd518645
Sergey Borovykh <bsn-iam@yandex.ru>
2018-06-01 22:23:34 +0300

    Added test for Yandex.Money logout

--- BrowserGames/YandexMoneyTest.cs
+++ BrowserGames/YandexMoneyTest.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
@@ -13,17 +10,81 @@ namespace BrowserGames
     [TestFixture]
     public class YandexMoneyTest
     {
-        private static KeyValuePair<string, string> validCredentials = new KeyValuePair<string, string>("sergey.borovykh", "AccountPassword");
-        public ChromeDriver Driver;
+        private const string validLogin = "sergey.borovykh";
+        private const string validPassword = "YaMGnKz";
+        public static ChromeDriver Driver;
 
-        //TODO Add different browsers as parameters
-        //TODO Fix current and check for different locales
+        //TODO Add support for different browsers
+        //TODO Check for different locales
         //TODO Think about signs of successfull or failed login
+        //TODO Think about excessive Asserts with error info
+
+        [Test, Timeout(30000)]
+        public void LogInTestPositive()
+        {
+            Driver = YandexMoneyLoginFromScratch(validLogin, validPassword);
+
+            var userNameSpans = Driver.FindElements(By.XPath("//span[@class='user__name']"));
+            Assert.IsTrue(userNameSpans.First().Text.Contains(validLogin), "Login sign was not found. Failed login?");
+        }
+
+        [Test, Timeout(30000)]
+        [TestCase("wrongLogin", validPassword)]
+        [TestCase(validLogin, "")]             
+        [TestCase(validLogin, "wrongPassword")]
+        public void LogInTestNegative(string login, string passwd)
+        {
+            Driver = YandexMoneyLoginFromScratch(login, passwd);
+
+            var enterAccountButton = Driver.FindElementsByClassName("passport-Button");
+            Assert.IsTrue(enterAccountButton.Any(), $"EnterAccount button was not found.");
+
+            waitUntil("//div[@class='passport-Domik-Form-Error']");
+        }
+
+        [Test, Timeout(30000)]
+        public void LogOutTest()
+        {
+            Driver = YandexMoneyLoginFromScratch(validLogin, validPassword);
+
+            var userNameSpans = Driver.FindElements(By.XPath("//span[@class='user__name']"));
+            userNameSpans.First().Click();
+            waitUntil("//div[@class='popup__content']");
+
+            var exitButtons = Driver.FindElementsByXPath("//*[contains(@class, 'user__logout')]");
+            Assert.IsTrue(exitButtons.Count == 1 && exitButtons.First().Displayed, "Logout button is not found.");
+
+            exitButtons.First().Click();
+            waitUntil("//a[@title= 'Log in']");
+
+            var loginButtons = Driver.FindElementsByXPath("//a[@title= 'Log in']");
+            Assert.IsTrue(loginButtons.Count == 1 && loginButtons.First().Displayed, "Login button is not found.");
+        }
+
+
+        private void waitUntil(string xpath)
+        {
+            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(5));
+            var errorFormDivShown = wait.Until(drv => drv.FindElement(By.XPath(xpath)).Enabled);
+        }
+
+        private ChromeDriver YandexMoneyLoginFromScratch(string login, string passwd)
+        {
+            var driver = GetDriver();
+            driver.Navigate().GoToUrl("https://money.yandex.ru");
+            var button = driver.FindElement(By.XPath("//a[@title= 'Log in']"));
+            button.Click();
+
+            driver.FindElementByName("login").SendKeys(login);
+            driver.FindElementByName("passwd").SendKeys(passwd);
+            driver.FindElementByClassName("passport-Button").Click();
+            return driver;
+        }
 
         public ChromeDriver GetDriver()
         {
             var options = new ChromeOptions();
-            //Options.AddArguments("headless");
+            //options.AddArguments("headless");
             options.AddArguments("window-size=1200x600");
             var chromeDriverService = ChromeDriverService.CreateDefaultService();
             chromeDriverService.HideCommandPromptWindow = true;
@@ -39,60 +100,9 @@ namespace BrowserGames
             // SetUp must exist to allow TearDown run.
         }
 
-        [Test, Timeout(30000)]
-        public void LogInTestPositive()
-        {
-            Driver = YandexMoneyLoginFromScratch(validCredentials);
-
-            var userNameSpans = Driver.FindElements(By.XPath("//span[@class='user__name']"));
-            Assert.IsTrue(userNameSpans.First().Text.Contains(validCredentials.Key), "Login sign was not found. Failed login?");
-        }
-
-        [Test, Timeout(30000)]
-        [TestCase("sergey.boovykh", "TestAccountPassword")] //wrong login
-        [TestCase("sergey.borovykh", "")]                   //empty password
-        [TestCase("sergey.boovykh", "testaccountpassword")] //wrong password
-        public void LogInTestNegative(string login, string passwd)
-        {
-            var credential = new KeyValuePair<string, string>(login, passwd);
-            Driver = YandexMoneyLoginFromScratch(credential);
-
-            var enterAccountButton = Driver.FindElementsByClassName("passport-Button");
-            Assert.IsTrue(enterAccountButton.Any(), $"EnterAccount button was not found.");
-
-            //Something strange with timeouts.
-            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(5));
-            var errorFormDivShown = wait.Until(drv => drv.FindElement(By.XPath("//div[@class='passport-Domik-Form-Error']")).Enabled);
-            Assert.IsTrue(errorFormDivShown, $"Error message div was not found (bool).");
-        }
-
-        [Test, Timeout(30000)]
-        public void LogOutTest()
-        {
-            Driver = YandexMoneyLoginFromScratch(validCredentials);
-
-            throw new NotImplementedException();
-        }
-
-
-        private ChromeDriver YandexMoneyLoginFromScratch(KeyValuePair<string, string> credentials)
-        {
-            var driver = GetDriver();
-            driver.Navigate().GoToUrl("https://money.yandex.ru");
-            var button = driver.FindElement(By.XPath("//a[@title= 'Log in']"));
-            button.Click();
-
-            driver.FindElementByName("login").SendKeys(credentials.Key);
-            driver.FindElementByName("passwd").SendKeys(credentials.Value);
-            driver.FindElementByClassName("passport-Button").Click();
-            return driver;
-        }
-
         [TearDown]
         public void ClearBrowser()
         {
-            //chromeDriver.GetScreenshot().SaveAsFile(@"D:\\GitHub\\BrowserGames\\BrowserGames\\screen.png", ScreenshotImageFormat.Jpeg);
-
             Driver.Quit();
         }
 
